71f457200a984d6272521dabf825d407c113e4fd
nick maxwell <nmaxwell@thoughtworks.com>
2016-08-11 17:35:02 -0400

    particle system added on palm contact, need to destroy instatiated game object

---
+++ Assets/HighFive.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighFive : MonoBehaviour {
+
+    public ParticleSystem effect;
+
+    void OnTriggerEnter(Collider collisionInfo)
+    {
+        if (collisionInfo.gameObject.name == "palm")
+        {
+            Debug.Log("palm detected collision with " + collisionInfo.gameObject.name);
+            effect.transform.position = gameObject.transform.position;
+            GameObject instatiatedParticle = Instantiate(effect, effect.transform.position, Quaternion.identity) as GameObject;
+            Destroy(instatiatedParticle,effect.duration);
+        }
+    }
+}
