4299dd1f3a338cdaab4a65013ab48398dd720c0e
Nick Maxwell <nmaxwell@thoughtworks.com>
2016-08-12 15:31:45 -0400

    gets high five working for other player

--- Assets/HighFive.cs
+++ Assets/HighFive.cs
@@ -1,25 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HighFive : Photon.MonoBehaviour {
+public class HighFive : Photon.MonoBehaviour
+{
 
     public GameObject effectPrimary;
     public GameObject effectSecondary;
 
     void OnTriggerEnter(Collider collisionInfo)
     {
-        if (collisionInfo.gameObject.name == "remotePalm") {
-            GameObject tempThinggie = collisionInfo.gameObject;
-            PhotonView tempThinggiePV = tempThinggie.GetComponent<PhotonView>();
-            if (tempThinggiePV.isMine)
-            {
-                Debug.Log("palm detected collision with " + collisionInfo.gameObject.name);
-                GameObject effectPrimaryInstance = Instantiate(effectPrimary, gameObject.transform.position, Quaternion.identity) as GameObject;
-                GameObject effectSecondaryInstance = Instantiate(effectSecondary, gameObject.transform.position, Quaternion.identity) as GameObject;
+        if (collisionInfo.gameObject.name == "remotePalm" &&
+            !collisionInfo.gameObject.GetComponent<PhotonView>().isMine)
+        {
+            Debug.Log("palm detected collision with " + collisionInfo.gameObject.name);
+            GameObject effectPrimaryInstance = Instantiate(effectPrimary, gameObject.transform.position, Quaternion.identity) as GameObject;
+            GameObject effectSecondaryInstance = Instantiate(effectSecondary, gameObject.transform.position, Quaternion.identity) as GameObject;
 
-                Destroy(effectPrimaryInstance, 2);
-                Destroy(effectSecondaryInstance, 2);
-            }
+            Destroy(effectPrimaryInstance, 2);
+            Destroy(effectSecondaryInstance, 2);
         }
     }
 }
