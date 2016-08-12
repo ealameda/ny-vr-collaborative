07fbc1c17e88df201cccb03c6cb3b19b2e5fa8e0
Allie Q <aquintano22@gmail.com>
2016-08-12 13:59:40 -0400

    changes high five trigger to remote palm of others only

--- Assets/HighFive.cs
+++ Assets/HighFive.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HighFive : MonoBehaviour {
+public class HighFive : Photon.MonoBehaviour {
 
     public GameObject effectPrimary;
     public GameObject effectSecondary;
 
     void OnTriggerEnter(Collider collisionInfo)
     {
-        if (collisionInfo.gameObject.name == "palm")
-        {
-            Debug.Log("palm detected collision with " + collisionInfo.gameObject.name);
-            GameObject effectPrimaryInstance = Instantiate(effectPrimary, gameObject.transform.position, Quaternion.identity) as GameObject;
-            GameObject effectSecondaryInstance = Instantiate(effectSecondary, gameObject.transform.position, Quaternion.identity) as GameObject;
+        if (collisionInfo.gameObject.name == "remotePalm") {
+            GameObject tempThinggie = collisionInfo.gameObject;
+            PhotonView tempThinggiePV = tempThinggie.GetComponent<PhotonView>();
+            if (tempThinggiePV.isMine)
+            {
+                Debug.Log("palm detected collision with " + collisionInfo.gameObject.name);
+                GameObject effectPrimaryInstance = Instantiate(effectPrimary, gameObject.transform.position, Quaternion.identity) as GameObject;
+                GameObject effectSecondaryInstance = Instantiate(effectSecondary, gameObject.transform.position, Quaternion.identity) as GameObject;
 
-            Destroy(effectPrimaryInstance, 2);
-            Destroy(effectSecondaryInstance, 2);
+                Destroy(effectPrimaryInstance, 2);
+                Destroy(effectSecondaryInstance, 2);
+            }
         }
     }
 }
